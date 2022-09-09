6b9b3f492825ce7a2a082a34920ed0680d25e7ba
Soare Catalin <stevensuncatalin369@gmail.com>
2022-09-09 12:32:22 +0300

    wrote what i did yesterday.

--- recap.matrici/mat.cpp
+++ recap.matrici/mat.cpp
@@ -3,6 +3,24 @@
 using namespace std;
 
 int main() {
-  cout << "Hello World" << endl;
+  int v[100][100];
+  int m, n, i, j;
+
+  cin >> m >> n;
+
+  cout << "citire" << endl; 
+  for (i = 1; i <= m; i++) {
+    for (j = 1; j <= n; j++) {
+      cin >> v[i][j];
+    }
+  }
+
+  cout << "scriere" << endl;
+  for (i = 1; i<= m; i++) {
+    for (j = 1;j <= n; j++) {
+      cout << v[i][j] << " ";
+    }
+    cout << endl;
+  }
   return 0;
 }
